c93425223407ab16ffb7673bd3991b7f35b70f39
MrForey <semenov.stepan.1985@gmail.com>
2025-07-30 06:04:41 +0600

    Initial commit

---
+++ src/main/java/net/mrforey/undergroundgnomes/UndergroundGnomes.java
@@ -0,0 +1,15 @@
+package net.mrforey.undergroundgnomes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UndergroundGnomes implements ModInitializer {
+	public static final String MOD_ID = "undergroundgnomes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/mrforey/undergroundgnomes/UndergroundGnomesClient.java
@@ -0,0 +1,10 @@
+package net.mrforey.undergroundgnomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UndergroundGnomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
